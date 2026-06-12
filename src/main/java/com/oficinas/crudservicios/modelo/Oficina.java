6b8c663c5c35ab5116a73fcfc26f8097e1602ad0
TeooProgrammer <mateojavieroscar@gmail.com>
2026-06-11 22:22:36 -0300

    oficinaentity

--- src/main/java/com/oficinas/crudservicios/modelo/Oficina.java
+++
@@ -1,5 +0,0 @@
-package com.oficinas.crudservicios.modelo;
-
-public class Oficina {
-    
-}
---
+++ src/main/java/com/oficinas/crudservicios/modelo/OficinaEntity.java
@@ -0,0 +1,47 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+package com.oficinas.crudservicios.modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(
+   name = "oficinas"
+)
+public class OficinaEntity {
+   @Id
+   @GeneratedValue(
+      strategy = GenerationType.IDENTITY
+   )
+   private Integer id;
+   @Column(
+      name = "nombre",
+      nullable = false,
+      length = 30,
+      unique = true
+   )
+   private String nombre;
+
+   public OficinaEntity() {
+   }
+
+   public Integer getId() {
+      return this.id;
+   }
+
+   public void setId(Integer id) {
+      this.id = id;
+   }
+
+   public String getNombre() {
+      return this.nombre;
+   }
+
+   public void setNombre(String nombre) {
+      this.nombre = nombre;
+   }
+}
